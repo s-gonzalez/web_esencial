45ae9e8733b6f5d99f74864d2d71224ad5046d10
s-gonzalez <seb.gonzalez.v@gmail.com>
2015-08-05 18:44:46 -0300

    Extras
    No usadas actualmente en la aplicación

---
+++ extras/mailchimp_old.php
@@ -0,0 +1,93 @@
+
+<!--Modal para contacto-->
+         
+            <div class="modal fade" id="mailchimp" tabindex="-1" role="dialog" aria-labelledby="relatorModalLabel" aria-hidden="true">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="relatorModalLabel" align="center">Formulario de Inscripción</h4>
+                  </div>
+                  <div class="modal-body">
+                  	
+                    <!-- Begin MailChimp Signup Form -->
+                    <div id="form" style="background:#fff;  clear:left; font:14px Helvetica,Arial,sans-serif;">
+						<form action="//grupoesencial.us11.list-manage.com/subscribe/post?u=985a5272f01dcd849f648943a&amp;id=8e40043c46" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class=" form-horizontal" target="_blank"  role="form">
+                        <div id="mc_embed_signup_scroll">
+                        
+                                    <br /><h2>Inscríbete en Santiago Despierta:</h2><br />
+                          
+                            <div class="form-group">
+                                <label for="mce-EMAIL" class="col-lg-3 control-label">Email</label>
+                                <div class="col-lg-8">
+                                  <input type="email" class="form-control" name="EMAIL" id="mce-EMAIL" required="required" placeholder="Email">
+                                </div>
+                            </div>
+                            
+                            <div class="form-group">
+                                <label for="mce-FNAME" class="col-lg-3 control-label">Nombre</label>
+                                <div class="col-lg-8">
+                                  <input type="text" value="" name="FNAME" required="required" class="form-control" id="mce-FNAME" placeholder="Nombre">
+                                </div>
+                            </div>
+                            
+                            <div class="form-group">
+                                <label for="mce-LNAME" class="col-lg-3 control-label">Apellido</label>
+                                <div class="col-lg-8">
+                                  <input type="text" value="" name="LNAME" required="required" class="form-control" id="mce-LNAME" placeholder="Apellido">
+                                </div>
+                            </div>
+                                     
+                            <div class="form-group">
+                                <label for="mce-PHONE" class="col-lg-3 control-label">Teléfono</label>
+                                <div class="col-lg-4">
+                                  <input type="text" name="PHONE" required="required" class="  form-control " value="" id="mce-PHONE" placeholder="Teléfono">
+                                </div>
+                            </div>
+                            
+                            <div class="form-group">
+                                <label for="mce-DESC" class="col-lg-3 control-label">Código de Descuento</label>
+                                <div class="col-lg-4">
+                                 <input type="text" value="" name="DESC" class="form-control " id="mce-DESC" placeholder="Código">
+                                
+                            	</div>
+                            </div>
+                            
+                            <div class="form-group">
+                                <label for="mce-MMERGE5" class="col-lg-3 control-label">Selecciona Valor</label>
+                                <div class="col-lg-8" align="left">
+                                	<label>
+                                  	  <input type="radio" value="$250.000 - Amigos Desafío de Humanidad*" name="MMERGE5" id="mce-MMERGE5-0"> $250.000 - Amigos Desafío de Humanidad*
+                                      </input>
+                                    </label><br />
+                                    <label>    
+                                  	  <input type="radio" value="$312.500 - Valor Normal" name="MMERGE5" id="mce-MMERGE5-1"> $312.500 - Valor Normal
+                                      </input>
+                                    </label>  	
+                                    <p align="justify" style="padding-top:15px;; font-size:11px; line-height: 15px;">* El valor para “Amigos de Desafío de Humanidad” se cumple al ser miembro activo de un Grupo de Desarrollo, al haber asistido a, al menos, un Encuentro de Lo Alto, al ser parte de la Comunidad de Organizaciones Sociales o al ser colaborador o miembro de alguna de las empresas que colaboran con Desafío. Esta condición será verificada con la propia institución, si es necesario, una vez inscrito el participante, pudiendo Grupo Esencial rechazar la inscripción si no se verifica el cumplimiento, devolviendo el pago efectuado al interesado.</p>
+                                    <div class="clear"><input type="submit" value="Enviar" name="subscribe" id="mc-embedded-subscribe" class="btn btnaction2">
+                                    </div>
+                            	</div>
+                            </div>
+                            
+                            <div id="mce-responses" class="clear">
+                                    <div class="response" id="mce-error-response" style="display:none"></div>
+                                    <div class="response" id="mce-success-response" style="display:none">
+                                    </div>
+                            </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                            <div style="position: absolute; left: -5000px;"><input type="text" name="b_985a5272f01dcd849f648943a_8e40043c46" tabindex="-1" value="">
+                            </div>
+                           
+                          </div>
+                    	</form>
+                 	</div>
+
+		<!--End mc_embed_signup-->
+                
+             
+                  </div><!--modal Body-->
+                </div><!--content-->
+              </div><!--Dialog-->
+            </div><!--Modal--->
+
+
---
+++ extras/relatores - Copia.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="assets/ico/favicon.png">
+
+    <title>SPOT - Free Bootstrap 3 Theme</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/main.css" rel="stylesheet">
+
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+     <?php 
+   include"navbar.php";
+   ?>
+
+	<!-- INFO SECTION -->
+    <a name="masinfo"></a>
+    <div class="container w">
+        <div class="row centered">
+            <div class="page-header">
+                <h2 style="COLOR: rgb(186, 10, 52)">RELATORES INVITADOS</h2>
+                <p>Destacados relatores de diferentes origenes y disciplinas han desarrollado actividades y temas de conversación y reflexión para presentar en <b>Santiago Despierta</b>, Encuentro de Experiencias, motivados por encontrarse con un público estimulado a explorar y abrirse a nuevas posibilidades de autoconocimiento.</p>
+            </div><!--page-header-->
+         </div><!--row-->
+         
+         <div class="row centered"> 
+                <br>
+                
+                <div class="col-sm-3">
+                <a href="#"><img class="img-circle" src="assets/img/relatores_final/ncruz.jpg" width="110" height="110" alt="Nicolás Cruz"></a>
+                <h4>Nicolás Cruz</h4>
+               
+            </div><!-- col-lg-3 -->
+            
+            <div class="col-sm-3">
+                <a href="#"><img class="img-circle" src="assets/img/relatores_final/pedro.jpg" width="110" height="110" alt="Pedro Arellano"></a>
+                <h4>Pedro Arellano</h4>
+        
+            </div><!-- col-lg-3 -->
+            
+            <div class="col-sm-3">
+                    <a href="#"><img class="img-circle" src="assets/img/relatores_final/cramirez.jpg" width="110" height="110" alt="Carolina Ramírez"></a>
+                <h4>Carolina Ramírez</h4>
+                
+            </div><!-- col-lg-3 -->
+
+            <div class="col-sm-3">
+                <a href="#"><img class="img-circle" src="assets/img/relatores_final/riveros.png" width="110" height="110" alt="Edgardo Riveros"></a>
+                <h4>Edgardo Riveros</h4>
+                
+            </div><!-- col-lg-3 -->
+
+            <div class="col-sm-3">
+                <a href="#"><img class="img-circle" src="assets/img/relatores_final/carlos.jpg" width="110" height="110" alt="Carlos Sanhueza"></a>
+                <h4>Carlos Sanhueza</h4>
+            </div>
+          </div>
+        <br>
+    	 <div class="row centered"> 
+            <br>
+            <div class="col-sm-3">
+                <a href="#"><img class="img-circle" src="assets/img/relatores_final/pedro.jpg" width="110" height="110" alt="Pedro Arellano"></a>
+                <h4>Pedro Arellano</h4>
+        
+            </div><!-- col-lg-3 -->
+
+            <div class="col-sm-3">
+                <a href="#"><img class="img-circle" src="assets/img/relatores_final/ian.png" width="110" height="110" alt="Ian Contreras"></a>
+                <h4>Ian Contreras</h4>
+          
+            </div><!-- col-lg-3 -->
+
+            <div class="col-sm-3">
+                <a href="#"><img class="img-circle" src="assets/img/relatores_final/vlad.jpeg" width="110" height="110" alt="Vladimir Huber"></a>
+                <h4>Vladimir Huber</h4>
+               
+            </div><!-- col-lg-3 -->
+
+            <div class="col-sm-3">
+                <a href="#"><img class="img-circle" src="assets/img/relatores_final/sergio.png" width="110" height="110" alt="Sergio Huneeus"></a>
+                <h4>Sergio Huneeus</h4>
+                
+            </div><!-- col-lg-3 -->
+            
+           
+        </div><!-- row -->
+        <br>
+        
+        <div class="row centered ">
+            <br>
+             <div class="col-sm-3">
+                <a href="#"><img class="img-circle" src="assets/img/relatores_final/izquierdo.png" width="110" height="110" alt="Pilar Izquierdo"></a>
+                <h4>Pilar Izquierdo</h4>
+                
+            </div><!-- col-lg-3 -->
+
+            <div class="col-sm-3">
+                <a href="#"><img class="img-circle" src="assets/img/relatores_final/martinez.png" width="110" height="110" alt="Daniel Martínez"></a>
+                <h4>Daniel Martínez</h4>
+                
+            </div><!-- col-lg-3 -->
+            
+            <div class="col-sm-3">
+                <a href="#"><img class="img-circle" src="assets/img/relatores_final/juancarlos.jpg" width="110" height="110" alt="Juan Carlos Obrador"></a>
+                <h4>Juan Carlos Obrador</h4>
+                
+            </div><!-- col-lg-3 -->
+
+            <div class="col-sm-3">
+                <a href="#"><img class="img-circle" src="assets/img/relatores_final/marco.jpg" width="110" height="110" alt="Marco A. Palma"></a>
+                <h4>Marco A. Palma</h4>
+            </div><!-- col-lg-3 -->
+        </div><!-- row -->
+        <br>
+        
+        <div class="row centered">
+            <br>
+            <div class="col-sm-3">
+                <a href="#"><img class="img-circle" src="assets/img/relatores_final/bruno.jpg" width="110" height="110" alt="Bruno Solari"></a>
+                <h4>Bruno Solari</h4>
+                
+            </div><!-- col-lg-3 -->
+
+            <div class="col-sm-3">
+                <a href="#"><a href="#"><img class="img-circle" src="assets/img/relatores_final/diego.png" width="110" height="110" alt="Diego Uribe"></a>
+                <h4 >Diego Uribe</h4>
+                
+            </div><!-- col-lg-3 -->
+
+            <div class="col-sm-3">
+                <a href="#"><img class="img-circle" src="assets/img/relatores_final/juanesteban.png" width="110" height="110" alt="Juan Esteban Varela"></a>
+                <h4>Juan Esteban Varela</h4>
+                
+            </div><!-- col-lg-3 -->
+
+            <div class="col-sm-3">
+                <a href="#"><img class="img-circle" src="assets/img/relatores_final/ward.JPG" width="110" height="110" alt="Caroline Ward"></a>
+                <h4>Caroline Ward</h4>
+               
+            </div><!-- col-lg-3 -->
+            
+        </div><!-- row -->
+    </div><!-- info section -->
+	
+    
+     <!--Modal for Relatores-->
+	<div class="modal fade" id="relatorModal" tabindex="-1" role="dialog" aria-labelledby="relatorModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="relatorModalLabel" align="center">Bruno Solari</h4>
+          </div>
+          <div class="modal-body">
+          <img class="img-rounded center-block" src="assets/img/relatores_final/bruno.jpg" width="200" height="200" alt="otro"></a><br>
+              <p align="justify">Director de Consultora Mindfulness, Miembro del Instituto del Bienestar y docente en Escuela de Psicología de la Universidad Adolfo Ibáñez. Catorce años de experiencia profesional en organizaciones, universidades y consultoras, introduciendo y fomentando la práctica de mindfulness. Dicta Talleres de Reducción de Estrés Basado en Atención Plena (REBAP/MBSR) desde 2012 y en paralelo se desempeña como psicoterapeuta en su consulta privada.Coorganizador del Primer Encuentro de Municipios por la Felicidad (Quillota, 2014).
+    Magíster en Psicología PUC (2010), Postítulo Terapia Dialéctico Conductual Universidad de Chile (2005) y Psicólogo Universidad Central (2000). Instructor de Mindfulness (formado por Fernando de Torrijos, 2011) y practicante de meditación budista (desde 1995).
+    Co-autor del capítulo Mindfulness y Bien-Estar del libro “La Felicidad: evidencias y experiencias para cambiar nuestro mundo”, 2013, Ediciones Sonepsyn. 
+             </p>
+                
+          </div>
+        </div><!--content-->
+      </div><!--Dialog-->
+	</div><!--Relatores-->
+	
+	<?php 
+	include"footer.html";
+	?>
+
+
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
